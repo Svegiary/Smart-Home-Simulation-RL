27c54dabd18c0cee8e92edfe1e3230fac20b3dec
Svegiary <haristsitsirigos@gmail.com>
2023-09-26 19:27:15 +0300

    dehumidifier

--- simulation/Simulation.py
+++ simulation/Simulation.py
@@ -1,6 +1,7 @@
 from models.Home.Home import Home
 from simulation.SnapshotDataCalculator import SnapshotDataCalculator
 from simulation.SimulationController import SimulationController
+from simulation.config.simulation_config import SimulationConfig
 from simulation.data_generation.simulation_data import SimulationData
 from simulation.data_generation.timestamp_generation.timestamp import TimestampGeneration
 from simulation.SimulationRuntime import *
@@ -13,13 +14,15 @@ class Simulation:
             timestamps: TimestampGeneration,
             simulation_data:  SimulationData,
             home: Home,
-            controller: SimulationController
+            controller: SimulationController,
+            config: SimulationConfig,
     ):
         self.timestamps = timestamps.timestamps
         self.simulation_data = simulation_data
         self.home = home
         self.simulation_runtime_plan = NoRuntime()
         self.controller = controller
+        self.config = config
 
     def set_runtime_plan(self, runtime_plan: SimulationRuntime):
         self.simulation_runtime_plan = runtime_plan
@@ -28,7 +31,6 @@ class Simulation:
         self.simulation_runtime_plan.start(self)
 
     def extract_snapshot(self, timestamp):
-        print(self.simulation_data.humidity_data)
         return SimulationSnapshot(
             self.simulation_data.temp_data[timestamp],
             self.simulation_data.humidity_data[timestamp],
---
+++ simulation/preferences/simulation_preferences.py
@@ -0,0 +1,23 @@
+from typing import Dict
+
+
+class Preferences:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Preferences, cls).__new__(cls)
+            cls._instance.initialize_preferences()
+        return cls._instance
+
+    def initialize_preferences(self):
+        self.target_temp = None
+        self.target_hum = None
+        self.target_luminance = None
+        self.target_noise = None
+
+    def set_preferences(self, preferences: Dict):
+        self.target_temp = preferences["target_temp"]
+        self.target_hum = preferences["target_humidity"]
+        self.target_luminance = preferences["target_luminance"]
+        self.target_noise = preferences["target_noise"]
